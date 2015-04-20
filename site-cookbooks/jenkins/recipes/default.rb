e2ceff0cd4e6c83c01c662477ae69423d7f192f4
Kawabata Shingo <nickel.app@gmail.com>
2015-04-21 03:09:40 +0900

    rails add

--- site-cookbooks/jenkins/recipes/default.rb
+++ site-cookbooks/jenkins/recipes/default.rb
@@ -1,8 +1,23 @@
-#
-# Cookbook Name:: jenkins
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# aptインストール
+package "apt" do
+	action :install
+end
+
+# Jenkins本体のインストール
+apt_repository "jenkins" do
+  uri "http://pkg.jenkins-ci.org/debian binary/"
+  # 正規でないリポジトリをアップデートするときの証明
+  # 公開鍵
+  key "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+  retries 10
+  retry_delay 10
+  action :add
+end
+
+package "jenkins" do
+  action :install
+end
+
+service "jenkins" do
+  action [:enable, :start]
+end
